d28a788f3c6f3ee9d2ced791944055f941032c8f
Praveen Kumar B <praveenkk21@gmail.com>
2025-04-12 13:23:40 +0530

    with class based view

--- students/students/urls.py
+++ students/students/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from .views import student_sub_details
 
 urlpatterns = [
     path('', views.student_list, name='student-list'),
     path('/<int:pk>/', views.student_detail, name='student-detail'),
-    path('/studentsub/', views.student_sub_details,name='student-sub-details')
+    path('studentsub/', student_sub_details.as_view(),name='student-sub-details')
 ]
--- students/students/views.py
+++ students/students/views.py
@@ -1,4 +1,6 @@
+import django.shortcuts
 from rest_framework.decorators import api_view
+from rest_framework.decorators import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import StudentSub
@@ -42,14 +44,14 @@ def student_detail(request, pk):
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view(['GET', 'POST'])
-def student_sub_details(request):
-    if request.method == 'GET':
+
+class student_sub_details(APIView):
+    def get(self,request):
         students = StudentSub.objects.all()
         serializer = StudentSubSerializer(students, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request):
         sid = request.data.get('sid')
         serializer = StudentSubSerializer(data=request.data)
         # Check if student exists
@@ -60,3 +62,24 @@ def student_sub_details(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+        
+# @api_view(['GET', 'POST'])
+# def student_sub_details(request):
+#     if request.method == 'GET':
+#         students = StudentSub.objects.all()
+#         serializer = StudentSubSerializer(students, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         sid = request.data.get('sid')
+#         serializer = StudentSubSerializer(data=request.data)
+#         # Check if student exists
+#         if not Student.objects.filter(id=sid).exists():
+#             return Response({'error': 'Student with given ID does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+            
+#         elif serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
